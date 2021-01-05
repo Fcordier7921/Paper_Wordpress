397d0a7c151cc0f1491bdd42323fbd4cd531ce94
Fcordier7921 <f.cordier@codeur.online>
2021-01-05 17:11:05 +0100

    latest work

--- wp-content/themes/paper/footer.php
+++ wp-content/themes/paper/footer.php
@@ -1,16 +1,19 @@
-<footer id="footer">
-	<div class="newsletter">
-		<form>
-			<input type="text" placeholder="Sign up ou newsletter">
-			<input type="submit" value="envoyer">
-	</form>
-	</div>
-	<div class="social">
-		<a href="/" target="blank"><img src="ds" /></a>
-		<a href="/" target="blank"><img src="sdf" /></a>
-	</div>
+</div><!--div de la classe row dans la header-->
+</div><!--div du container dans la header-->
+<footer id="footer" class="container-fluid" style="background-color: gray;">
+	
 </footer>
-</div>
+
 <?php wp_footer(); ?>
+<script>
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+</script>
 </body>
 </html>
--- wp-content/themes/paper/template_accueil.php
+++ wp-content/themes/paper/template_accueil.php
@@ -2,19 +2,26 @@
 
 <?php get_header(); ?>
 
-
-<div class="container">
+<div class="contenu col-12 col-md-7">
+  <div>
+  <div class="titletrait mt-5">
+    <h5 class="title">LATEST <span>WORK</span></h5>
+    <div class="trait"><hr></div>
+  </div>
   <div class="row">
-    <div class="col-sm">
-      One of three columns
-    </div>
-    <div class="col-sm">
-      One of three columns
-    </div>
-    <div class="col-sm">
-      One of three columns
-    </div>
+  <?php
+  $palnel=get_field( "images" );
+
+  for ($i=0; $i < count($palnel) ; $i++) {
+    echo "<div class = 'col-12 col-md-3 my-2'>";
+    echo "<img src ='".$palnel[$i]['images']["sizes"]["thumbnail"]."' class='img-fluid' />";
+    echo "<p>".$palnel[$i]['non']."</p>";
+    echo "</div>";
+    }
+  ?>
   </div>
 </div>
+</div>
+
 
 <?php get_footer(); ?>
